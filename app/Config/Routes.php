7e1207361745d280b43ac5810cf7a1e221285962
Rama <rambokece03@gmail.com>
2026-01-07 14:28:13 +0700

    Pesan commit Anda

---
+++ app/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('barang', 'Barang::index');
+$routes->get('barang/tambah', 'Barang::tambah');
+$routes->get('barang/edit/(:any)', 'Barang::edit/$1');
+$routes->post('barang/add', 'Barang::add');
+$routes->post('barang/update', 'Barang::update');
+$routes->get('barang/hapus/(:any)', 'Barang::hapus/$1');
